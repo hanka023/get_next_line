6ddfcd32e3523a4bad82f9e18b328b5a86459f7c
hanka023 <128853526+hanka023@users.noreply.github.com>
2025-09-23 15:32:59 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -5,32 +5,55 @@
 #include <stdlib.h>  // exit()
 #include "get_next_line.h"
 
+// char 	*read_file(int fd)
+// {
+// 	ssize_t byte_read;
+// 	char *buf[BUFFER_SIZE + 1];
+// 	static char *buffer = NULL;
+// 	char tmp;
+
+
+// 	byte_read = read(fd, buf, BUFFER_SIZE);
+// 	buf[byte_read] = '\0';
+
+// 	tmp = strjoin(buffer, buf);
+// 	free (buffer);
+// 	buffer = tmp;
+// 	return (tmp);
+// }
+
 
 // char *get_next_line(int fd)
 // {
-// 	char *buf;
-
-// 	static char *buffer = NULL;
-// 	ssize_t n;
-// 	char *tmp;
-
-// 	buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-// 	n = read(fd, buf, BUFFER_SIZE);
-// 	if (n <= 0)
-// 			return NULL;
-// 	else if (n > 0)
-// 	{
-// 		buf[n] = '\0';   // ← tady přidám ukončovací znak
-// 	}
-
-// 	if (buffer == NULL)           // ← první volání
-// 			tmp = ft_strdup(buf);
-// 	else
-// 	tmp = ft_strjoin(buffer, buf);
-// 	free(buffer);
-// 	buffer = tmp;
-
-// 	return (tmp);
+//     static char *buffer;
+//     // ... rest of the function
 // }
+
+char *get_next_line(int fd)
+{
+	char *buf;
+	static char *buffer;
+	ssize_t n;
+	char *tmp;
+
+	buffer = NULL;
+	buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	n = read(fd, buf, BUFFER_SIZE);
+	if (n <= 0)
+			return NULL;
+	else if (n > 0)
+	{
+		buf[n] = '\0';   // ← tady přidám ukončovací znak
+	}
+	if (buffer == NULL)           // ← první volání
+			tmp = ft_strdup(buf);
+	else
+	tmp = ft_strjoin(buffer, buf);
+	free(buffer);
+	buffer = tmp;
+	return (tmp);
+}
+
+
 
 
