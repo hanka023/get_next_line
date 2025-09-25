acb2b974f020e6dcae41f4e71b9b6212936c878e
hanka023 <128853526+hanka023@users.noreply.github.com>
2025-09-25 18:46:39 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,58 +1,135 @@
 
-#include <fcntl.h>   // open()
-#include <unistd.h>  // read(), close()
-#include <stdio.h>   // printf()
-#include <stdlib.h>  // exit()
 #include "get_next_line.h"
-
-// char 	*read_file(int fd)
-// {
-// 	ssize_t byte_read;
-// 	char *buf[BUFFER_SIZE + 1];
-// 	static char *buffer = NULL;
-// 	char tmp;
+#include <fcntl.h>  // open()
+#include <stdio.h>  // printf()
+#include <stdlib.h> // exit()
+#include <unistd.h> // read(), close()
 
 
-// 	byte_read = read(fd, buf, BUFFER_SIZE);
-// 	buf[byte_read] = '\0';
 
-// 	tmp = strjoin(buffer, buf);
-// 	free (buffer);
-// 	buffer = tmp;
-// 	return (tmp);
-// }
-
-
-// char *get_next_line(int fd)
-// {
-//     static char *buffer;
-//     // ... rest of the function
-// }
-
-char *get_next_line(int fd)
+char	*byte_read(int fd)
 {
-	char *buf;
-	static char *buffer;
-	ssize_t n;
-	char *tmp;
+	char	*buf;
+	ssize_t	n;
 
-	buffer = NULL;
 	buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!buf)
+		return (NULL);
 	n = read(fd, buf, BUFFER_SIZE);
 	if (n <= 0)
-			return NULL;
+	{
+		free(buf);
+		return (NULL);
+	}
 	else if (n > 0)
 	{
-		buf[n] = '\0';   // ← tady přidám ukončovací znak
+		buf[n] = '\0'; // ← tady přidám ukončovací znak
 	}
-	if (buffer == NULL)           // ← první volání
-			tmp = ft_strdup(buf);
-	else
-	tmp = ft_strjoin(buffer, buf);
-	free(buffer);
-	buffer = tmp;
-	return (tmp);
+	/**************************test**************** */
+	// printf("***********  buf = %s \n\n", buf);
+	/**************************test**************** */
+	return (buf);
 }
+
+char	*join_buffer(int fd)
+{
+	char		*new_line;
+	static char	*buffer;
+	char		*joined;
+	char		*tmp;
+
+	new_line = NULL;
+	while (new_line == NULL)
+	{
+		// printf(" ------------------nove while ------------ \n\n");
+		tmp = byte_read(fd);
+		new_line = ft_strrchr(tmp, '\n');
+		// printf("***********  tmp = %s \n\n", tmp);
+		// printf(" ********* new_line = %s \n\n", new_line);
+		// printf(" ********* buffer pred upravou  = %s \n\n", buffer);
+		if (!buffer)
+			buffer = ft_strdup(tmp); // první alokace
+		else
+		{
+			joined = ft_strjoin(buffer, tmp);
+			free(buffer);
+			buffer = joined;
+		}
+		// printf(" ********* buffer posledni = %s \n\n", buffer);
+	}
+	// printf(" ********* buffer hotovy = %s \n\n", buffer);
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	char	*line;
+	int		i;
+	int		n;
+	char	*newline;
+	char	*res;
+
+	i = 0;
+	n = 0;
+	res = 0;
+	line = join_buffer(fd);
+	if (!line)
+		return (NULL);
+	while (line[i] != '\n' && line[i] != '\0')
+		i++;
+	newline = malloc((sizeof(char)) * (i + 1));
+	while (n < i)
+	{
+		newline[n] = line[n];
+		++n;
+	}
+	newline[n] = '\0';
+	return (newline);
+}
+
+
+char *start_line(int fd)
+{
+
+ static char *buffer;
+char *line;
+char *newline;
+int l;
+int n;
+
+
+
+line = join_buffer(fd);
+    if (!line)
+        return (NULL);
+l = (int)ft_strlen(line);
+//printf(" *********  line = %s \n\n", line);
+
+newline = get_next_line(fd);
+	  if (!newline)
+        return (NULL);
+n = (int)ft_strlen(newline);
+
+
+buffer = line;
+
+//printf(" *res= = %s \n\n", line);
+// printf(" *newline = %s \n\n", newline);
+buffer[n] = '\0';
+
+//free(line);
+
+free(newline);
+return(buffer);
+
+}
+
+
+
+
+
+
+
 
 
 
