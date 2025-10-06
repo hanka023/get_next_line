8bf6fc4f85d9e343380f3ca209606b13ed1cce39
hanka023 <128853526+hanka023@users.noreply.github.com>
2025-10-06 21:55:25 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,24 +1,27 @@
-
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
-#include <fcntl.h>   // open()
-#include <unistd.h>  // read(), close()
-#include <stdio.h>   // printf()
-#include <stdlib.h>  // exit()
-
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 2
+#define BUFFER_SIZE 10
 #endif
 
-char	*get_next_line(int fd);
-char	extract_line (char **buffer);
 
 /*  utils */
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_strrchr(const char *str, int c);
+char	*ft_strchr(const char *str, int c);
 char	*ft_strdup(const char *str);
 void	*ft_calloc(size_t count, size_t size);
+
+/**************moje*************** */
+char	*byte_read(int fd,char **buffer );
+char	*join_buffer(char *buffer, int fd);
+char	*next_line(char *line);
+char	*start_line(char *buffer);
+char	*get_next_line (int fd);
 
 #endif
