ff271255e1665729779011d175514cd2fe2bb69c
hanka023 <128853526+hanka023@users.noreply.github.com>
2025-10-07 00:12:32 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,14 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanka <hanka@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 13:37:42 by haskalov          #+#    #+#             */
+/*   Updated: 2025/10/06 23:46:19 by hanka            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+# define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 10
-#endif
-
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
 /*  utils */
 size_t	ft_strlen(const char *s);
@@ -16,12 +27,11 @@ char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strdup(const char *str);
 void	*ft_calloc(size_t count, size_t size);
-
-/**************moje*************** */
-char	*byte_read(int fd,char **buffer );
+/**************functions*************** */
+char	*byte_read(int fd, char **buffer);
 char	*join_buffer(char *buffer, int fd);
 char	*next_line(char *line);
 char	*start_line(char *buffer);
-char	*get_next_line (int fd);
+char	*get_next_line(int fd);
 
 #endif
